63e85d2db25abbb2a66aee66514ef528d0f096fe
enorthcutt <enorthcutt1985@gmail.com>
2025-06-01 02:07:54 -0500

    Update merge-request.sh

--- merge-request.sh
+++ merge-request.sh
@@ -11,7 +11,7 @@ fi
 [[ $CI_PROJECT_URL =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
 
 # Look which is the default branch
-TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}" | jq --raw-output '.default_branch'`;
+TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${#GITLAB_PRIVATE_TOKEN}" | jq --raw-output '.default_branch'`;
 
 # The description of our new MR, we want to remove the branch after the MR has
 # been closed
